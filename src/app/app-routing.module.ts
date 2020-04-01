acdc83092547916048a7f33e35f0b7683f0bbd08
Unear <andres_mau.gutierrez@uao.edu.co>
2020-04-01 14:53:43 -0500

    Version calendario 0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 import { LoginComponent } from './login/login.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 
 const routes: Routes = [
@@ -9,10 +10,13 @@ const routes: Routes = [
     path:'', redirectTo: 'login', pathMatch: 'full' /* El path vacio significa que es la primera en mostrarse, el redirectTo significa que si esta vacio lo redirecciona a ese componente */
   },
   {
-    path:'login', component: LoginComponent
+    path:'login', component: LoginComponent /* Importo componente "login" */
   },
   {
-    path:'pagina-principal', component: PaginaPrincipalComponent
+    path:'pagina-principal', component: PaginaPrincipalComponent /* Importo componente "pagina principal" */
+  },
+  {
+    path:'calendario', component: CalendarioComponent /* Importo componente "calendario" */
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { CalendarioComponent } from './calendario/calendario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PaginaPrincipalComponent
+    PaginaPrincipalComponent,
+    CalendarioComponent
   ],
   imports: [
     BrowserModule,
