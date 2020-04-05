2979cb1712a9c33f1f3472bb5a8bb752480ac5b9
Camilo Rodriguez <juancamilo9607@gmail.com>
2020-04-05 20:47:17 +0100

    Cambios frontend del login

---
+++ src/app/eliminar-emprendedor/eliminar-emprendedor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-emprendedor',
+  templateUrl: './eliminar-emprendedor.component.html',
+  styleUrls: ['./eliminar-emprendedor.component.scss']
+})
+export class EliminarEmprendedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
