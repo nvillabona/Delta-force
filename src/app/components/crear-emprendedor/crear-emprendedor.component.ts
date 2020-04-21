1a79ba8421c083a39e2c25f18c23730875ad7b2c
camilor96 <juancamilo9607@gmail.com>
2020-04-21 18:35:07 -0500

    Componente crear emprendedor conectado con el servidor e insertando y componente comprendedores conectado al servidor y listando

--- src/app/components/crear-emprendedor/crear-emprendedor.component.ts
+++ src/app/components/crear-emprendedor/crear-emprendedor.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Emprendedor } from "../../models/emprendedor";
+import { LoginUsuario } from "../../models/login.usuario";
+import { EmprendedorService } from "../../services/emprendedor.service";
 
 @Component({
   selector: 'app-crear-emprendedor',
   templateUrl: './crear-emprendedor.component.html',
-  styleUrls: ['./crear-emprendedor.component.scss']
+  styleUrls: ['./crear-emprendedor.component.scss'],
+  providers:[EmprendedorService]
 })
 export class CrearEmprendedorComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public emprendedor: Emprendedor;
+  public status: string;
+
+  constructor(
+    private _emprendedorService: EmprendedorService
+  ) { 
+    this.title = "Crear Emprendedores";
+    this.emprendedor= new Emprendedor(0,'','','','','','', 0, '', '',0,'','','');
+    this.status = "success"
+  }
 
   ngOnInit() {
+
+  }
+
+  onSubmit(form){
+    console.log(this.emprendedor);
+    this._emprendedorService.saveEmprendedor(this.emprendedor).subscribe(
+      response => {
+        if(response.emprendedor){
+          this.status = "success";
+          form.reset();
+        }else {
+          this.status = "failed";
+        }
+        
+      },
+      error => {
+        console.log(error);
+        
+      }
+    );
+    
   }
 
 }
--- src/app/components/emprendedores/emprendedores.component.ts
+++ src/app/components/emprendedores/emprendedores.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Emprendedor } from "../../models/emprendedor";
+import { EmprendedorService } from "../../services/emprendedor.service";
+
 
 @Component({
   selector: 'app-emprendedores',
   templateUrl: './emprendedores.component.html',
-  styleUrls: ['./emprendedores.component.scss']
+  styleUrls: ['./emprendedores.component.scss'],
+  providers:[EmprendedorService]
+  
 })
 export class EmprendedoresComponent implements OnInit {
+  public emprendedores: Emprendedor[];
 
-  constructor() { }
+  constructor(
+    private _emprededorService: EmprendedorService
+  ) {
+   }
 
   ngOnInit() {
+    this.getEmprendedores();
+  }
+
+  getEmprendedores(){
+    this._emprededorService.getEmprendedores().subscribe(
+      response =>{
+        console.log(response);
+        if(response){
+        
+          this.emprendedores = response;
+          console.log(this.emprendedores);
+          
+        }else{
+          
+        }
+        
+      },
+      error => {
+        console.log(error);
+        
+      }
+    );
   }
 
 }
