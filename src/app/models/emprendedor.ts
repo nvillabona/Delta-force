22731cfae8f6990b36c2414148498c98581484ff
camilor96 <juancamilo9607@gmail.com>
2020-04-22 16:08:27 -0500

    comit de lo anterior

---
+++ src/app/models/emprendedor.ts
@@ -0,0 +1,21 @@
+export class Emprendedor{
+    constructor(
+        public cedula_emprendedor: number,
+        public nombres: string,
+        public apellidos: string,
+        public direccion: string,
+        public programa_academico: string,
+        public profesion: string,
+        public area_enfoque: string,
+        public telefono: number,
+        public perfil_academico: string,
+        public facultad: string,
+        public codigo: number,
+        public email: string,
+        public password: string,
+        public rol: string,
+
+    ){
+
+    }
+}
