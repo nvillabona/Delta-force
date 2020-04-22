9545207611aba6393ed44bdcd7efa9576be36a8b
Unear <andres_mau.gutierrez@uao.edu.co>
2020-04-21 19:37:46 -0500

    Version empredimiento, crear y detalle hechos (2)

---
+++ src/app/components/detalle-emprendimiento/detalle-emprendimiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-emprendimiento',
+  templateUrl: './detalle-emprendimiento.component.html',
+  styleUrls: ['./detalle-emprendimiento.component.scss']
+})
+export class DetalleEmprendimientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
