072c69a79ce69c9a3bafec43ba422f5eed26ece5
camilor96 <juancamilo9607@gmail.com>
2020-04-23 01:49:00 -0500

    Componente consultores  y crear consutlores conectados al servidor , insertando y listando

--- src/app/components/eliminar-consultor/eliminar-consultor.component.ts
+++ src/app/components/eliminar-consultor/eliminar-consultor.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Facilitador } from "../../models/facilitador";
+import { FacilitadorService } from "../../services/facilitador.service";
+import { HttpHeaders } from '@angular/common/http';
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: 'app-eliminar-consultor',
   templateUrl: './eliminar-consultor.component.html',
-  styleUrls: ['./eliminar-consultor.component.scss']
+  styleUrls: ['./eliminar-consultor.component.scss'],
+  providers :[FacilitadorService]
 })
 export class EliminarConsultorComponent implements OnInit {
+  public nombre: string;
+  public apellido: string;
+  public cedula: number;
 
-  constructor() { }
+  constructor(
+    private _facilitadorService: FacilitadorService,
+    private _router: Router,
+    private _route: ActivatedRoute,
+  ) { }
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {
+    this._route.params.subscribe((params:Params)=>{
+      this.nombre = params.nombres;
+      this.apellido= params.apellidos;
+      this.cedula = params.cedula;
+      console.log(params);
+  });
+}
+deleteFacilitador(cedulaF){
+  this._facilitadorService.deleteFacilitador(cedulaF).subscribe(
+    response => {
+      if (response) {
+        this._router.navigate(['/consultores']);   
+      }
+    },
+    error => {
+      console.log(error);
+      
+    }
+  );
+
+
+    
+}
 
 }
---
+++ src/app/services/facilitador.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Facilitador } from "../models/facilitador";
+import { LoginUsuario } from "../models/login.usuario";
+import { Global } from "./global";
+
+@Injectable()
+
+export class FacilitadorService{
+
+    public url: string;
+
+    constructor(
+        private _http: HttpClient
+    ){
+        this.url= Global.url;
+    }
+
+    testService(){
+        return 'probando servicio de angular'
+    }
+
+    saveFacilitador(facilitador: Facilitador): Observable<any>{
+
+        let params = JSON.stringify(facilitador);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+
+        return this._http.post(this.url+'/facilitadores', params, {headers: headers})
+    }
+    getFacilitadores(): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.get(this.url+'/facilitadores', {headers:headers})
+    }
+    deleteFacilitador(cedula): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+    
+        return this._http.delete(this.url+'/facilitadores/'+cedula, {headers:headers})
+    }
+    getFacilitador(cedula): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        
+        return this._http.get(this.url+'/facilitadores/'+cedula, {headers: headers});
+    }
+
+    updateFacilitador(facilitador: Facilitador): Observable<any>{
+        let params = facilitador;
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+    
+        return this._http.put(this.url+'/facilitadores/'+facilitador.cedula_facilitador, params, {headers:headers})
+    }
+    
+}
+
