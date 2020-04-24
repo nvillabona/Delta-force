10c0ba5a83d8ba1fe0792502ee44618d89cb1865
camilor96 <juancamilo9607@gmail.com>
2020-04-24 02:25:34 -0500

    Componente de crear emprendimiento funcionando conectado con la BD y creando

---
+++ src/app/models/emprendimiento.ts
@@ -0,0 +1,27 @@
+export class Emprendimiento{
+    constructor(
+        public consecutivo: number,
+        public titulo: string,
+        public c_emprendedor: number,
+        public programa_academico: string,
+        public facultad: string,
+        public egresado: string,
+        public sector: string,
+        public etapa: string,
+        public NIT: number,
+        public fecha_creacion: string,
+        public num_empleados: number,
+        public num_contacto: number,
+        public propuesta_valor: string,
+        public segmento_clientes:string,
+        public canales: string,
+        public fuente_ingresos: string,
+        public recursos_clave: string,
+        public actividades_clave:string,
+        public socios_clave: string,
+        public estructura_costos: string
+
+    ){
+
+    }
+}
