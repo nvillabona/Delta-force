22220859d7b39eb5975369f216ca1a4b38562a54
camilor96 <juancamilo9607@gmail.com>
2020-04-26 04:18:16 -0500

    Calendario en español

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid'; /* Importado manualmente */
+import esLocale from '@fullcalendar/core/locales/es';
+import { ConsultoriaCalendar } from "../../models/consultoria";
+import { ConsultoriaService } from "../../services/consultoria.service";
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
-  styleUrls: ['./calendario.component.scss']
+  styleUrls: ['./calendario.component.scss'],
+  providers:[ConsultoriaService]
 })
 export class CalendarioComponent implements OnInit {
+  public locales = [esLocale];
 
   calendarPlugins = [dayGridPlugin]; /* Importado manualmente */
-  constructor() { }
+  constructor(
+    private _consultoriaService: ConsultoriaService
+  ) { }
 
   ngOnInit() {
   }
